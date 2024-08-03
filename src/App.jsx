6740559ee243a3c8ccbcc5faf1ee1a708c93d50f
Tushar Bhos <tusharbhos3412@gmail.com>
2024-08-03 10:36:22 +0530

    folder setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,22 @@ import Footer from "./components/Footer";
 import Registration from "./components/registration/Registration";
 import Login from "./components/Login/Login";
 import "./App.css";
+import "./css/output.css";
+
 
 const App = () => {
   return (
     <Router>
       <div className="App">
         <div className="content">
-          <h1>Welcome to Aavai Admin Panel</h1>
-          <Link to="/registration">
+          <h1 className="text-3xl font-bold underline bg-green-500">Welcome to Aavai Admin Panel</h1>
+        
+          {/* <Link to="/registration">
             <button className="register-button">Register</button>
           </Link>
           <Link to="/login">
             <button className="login-button">Login</button>
-          </Link>
+          </Link> */}
           <Routes>
             <Route path="/registration" element={<Registration />} />
             <Route path="/login" element={<Login />} />
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,21 +5,9 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <p>&copy; 2024 Aavai. All rights reserved.</p>
-        <ul className="footer-links">
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">About</a>
-          </li>
-          <li>
-            <a href="#">Services</a>
-          </li>
-          <li>
-            <a href="#">Contact</a>
-          </li>
-        </ul>
+        <p>&copy; 2024 <a href="#">Aavai Tech pvt. ltd.</a> All rights reserved.<a href="mailto:aavaitech@gmail.com">aavaitech@gmail.com</a>
+        </p>
+        
       </div>
     </footer>
   );
